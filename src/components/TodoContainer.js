a357cba49000a72a6b225e8deb143d8cb4430c02
borgnine <derlrobbins@gmail.com>
2021-03-03 11:31:02 -0500

    adds component files

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class TodoContainer extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>whatever</h1>
+                <p>I am a react Component!</p>
+            </div>
+        )
+    }
+}
+
+export default TodoContainer
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react" 
+import ReactDom from "react-dom"
+import TodoContainer from "./components/TodoContainer"
+
+ReactDom.render(<TodoContainer />, document.getElementById( 'root' ))
